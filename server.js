e824084afde126a1f75ce24094a544cf915c79e2
mayowadavid <34623610+mayowadavid@users.noreply.github.com>
2022-01-12 21:19:34 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const { resolve } = require("path");
+const env = require("dotenv").config({ path: ".env" });
+// This is your test secret API key.
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+//const stripe = require('stripe')('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');
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.get("/", (req, res) => {
+  // Display checkout page
+  const path = resolve("public/checkout.html");
+  res.sendFile(path);
+});
+
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        //price: "price_1KGsHCLJV0ISPCLdGJGTjDq3",
+        price: process.env.PRICES,
+        quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
